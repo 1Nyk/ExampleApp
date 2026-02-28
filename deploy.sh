273323a2a64dbb5e75970a8a3c55d067a4db844e
Nyko77771 <nykytamcdonald@gmail.com>
2026-02-28 10:35:59 +0000

    added cd

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt update && sudo apt install nodejs npm
+# Install pm2 - production process manager for Node.js
+sudo npm install -g pm2
+# stop any instance of our application running currently
+pm2 stop ExampleApp
+# change directory into folder where app is located
+cd ExampleApp/
+# Install app dependancies
+npm install
+# Start the app with the process name example_app using pm2
+pm2 start ./bin/www --name example_app
